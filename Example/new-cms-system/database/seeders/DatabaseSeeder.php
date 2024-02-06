f8392a683912f8e631b199c977dc4fd0f7446e4a
JosteXD12 <jostecortes12@gmail.com>
2024-02-06 16:37:36 -0600

    modificacion de blog y post del proyecto
    mejora visuales, sacando informacion de la base de datos ademas de agregar nueva rutas para futuras mojeras

--- Example/new-cms-system/database/seeders/DatabaseSeeder.php
+++ Example/new-cms-system/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\User::factory(10)->create()
+            ->each(function ($user) {
+                $user->posts()->save(Factory::factoryForModel('App\Models\Post')->make());
+            });
     }
 }
--- Example/new-cms-system/routes/web.php
+++ Example/new-cms-system/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Post;
 
 
 
@@ -10,5 +11,11 @@ use Illuminate\Support\Facades\Auth;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/admin', [App\Http\Controllers\AdminsController::class, 'index'])->name('admin.index');
-Route::get('/post', [App\Http\Controllers\PostController::class, 'show'])->name('post');
+Route::get('/post/{post}', [App\Http\Controllers\PostController::class, 'show'])->name('post');
+
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/admin', [App\Http\Controllers\AdminsController::class, 'index'])->name('admin.index');
+    Route::post('/admin/posts', [App\Http\Controllers\PostController::class, 'store'])->name('post.store');
+});
