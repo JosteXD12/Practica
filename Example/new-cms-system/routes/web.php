2fb4e3c2110284a9837500b6eb8772a1c7c0afc5
JosteXD12 <jostecortes12@gmail.com>
2024-02-06 11:07:10 -0600

    Creación de la fase administrativa
    implemetación de templates para la creación de la parte administrativa y de post ademas de agregación de seguridad

---
+++ Example/new-cms-system/routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+
+
+
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin', [App\Http\Controllers\AdminsController::class, 'index'])->name('admin.index');
+Route::get('/post', [App\Http\Controllers\PostController::class, 'show'])->name('post');
