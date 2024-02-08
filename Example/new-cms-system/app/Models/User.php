8012bbb51e1b64ef38073e04f646f37f548a261a
JosteXD12 <jostecortes12@gmail.com>
2024-02-08 16:23:47 -0600

    Implemetacion de roles
    Se trabajo en el dashboard solo para que los que so admin pueda ingresar ahi

--- Example/new-cms-system/app/Models/User.php
+++ Example/new-cms-system/app/Models/User.php
@@ -42,15 +42,26 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-    public function posts(){
+    public function posts()
+    {
         return $this->hasMany(Post::class);
     }
 
-    public function permissions(){
+    public function permissions()
+    {
         return $this->belongsToMany(Permission::class);
     }
     public function roles()
     {
         return $this->belongsToMany(Role::class);
+    }
+
+    public function userHasRole($role_name)
+    {
+        foreach ($this->roles as $role) {
+            if ($role_name == $role->name)
+            return true;
+        }
+       return false;
     }
 }
